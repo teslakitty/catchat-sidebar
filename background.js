4159a0c05c8430856684a4d19d52d5f12705fc92
James <jameskuyt655@gmail.com>
2025-04-26 21:59:38 +0000

    implement side panel opening on action click and update side panel title

--- background.js
+++ background.js
@@ -1,1 +1,11 @@
-console.log("Background service worker loaded.");
+chrome.action.onClicked.addListener(async (tab) => {
+    try {
+      await chrome.sidePanel.open({
+        tabId: tab.id
+      });
+    } catch (error) {
+      console.error("Error opening side panel:", error);
+    }
+  });
+  
+  console.log("Background service worker loaded.");
